9232fdbaa36c311e245111973f837ad3f63a74d2
Nicolas <nicolas.henry@heig-vd.ch>
2020-11-05 11:54:09 +0100

    Add user entries

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 Nom du fichier : main.cpp
 Auteur(s) : Alexandre Junod, Jarod Streckeisen, Nicolas Henry
 Date creation : 05.11.2020
-Description : <à compléter>
-Remarque(s) : <à compléter>
+Description : Ce programme permet d'afficher des pages de calendrier entre 2 dates
+              qui seront entrées par l'utilisateur.
+Remarque(s) : Les données utilisateurs ne comprennent pas le jour.
+tutu : entrer les variables sur une seule ligne
 Compilateur : Mingw-w64 g++ 8.1.0
 -----------------------------------------------------------------------------------
 */
@@ -14,6 +16,26 @@ Compilateur : Mingw-w64 g++ 8.1.0
 using namespace std;
 
 int main() {
+	unsigned int firstMonth, firstYear, lastMonth, lastYear;
+	bool saisieOK2;
+	do {
+		cout << "Entrez la date de debut [mm aaaa] : ";
+		cin >> firstMonth >> firstYear;
+		if (firstMonth <= 0) {
+			cin.clear(),
+				cout << "Saisie incorrecte" << endl;
+		}
+		saisieOK2 = false;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	} while (saisieOK2);
+	/*
+	cout << "Entrez la date de debut [mm aaaa] : ";
+	cin >> firstMonth >> firstYear;
+	cout << "Entrez la date de fin [mm aaaa] : ";
+	cin >>  lastMonth >> lastYear;
 
- return EXIT_SUCCESS;
+	cout << firstMonth << " " << firstYear;
+	cout << lastMonth << " " << lastYear;
+	*/
+	return EXIT_SUCCESS;
 }
