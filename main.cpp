9334172f2fab7447c385f791a0d3d2ed29f9e4a4
Nicolas <nicolas.henry@heig-vd.ch>
2020-11-05 11:22:18 +0100

    Add default code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : main.cpp
+Auteur(s) : Alexandre Junod, Jarod Streckeisen, Nicolas Henry
+Date creation : 05.11.2020
+Description : <à compléter>
+Remarque(s) : <à compléter>
+Compilateur : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
 #include <iostream>
+using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+
+ return EXIT_SUCCESS;
 }
