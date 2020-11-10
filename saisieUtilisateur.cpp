b051fe102e697532a838f2165ddf514e526130a0
Jarod <jarod.streckeisen@heig-vd.ch>
2020-11-10 17:33:56 +0100

    format code, change saisieutilisateur

--- saisieUtilisateur.cpp
+++ saisieUtilisateur.cpp
@@ -14,26 +14,28 @@ Compilateur : Mingw-w64 g++ 8.1.0
 
 using namespace std;
 
-void saisieDate(unsigned& mois, unsigned& annee, const string& message) {
+void saisieDate(unsigned &mois, unsigned &annee, const string &message) {
    bool estValide = false;
    do {
       cout << message;
 
       cin >> mois;
 
-		if (cin.peek() == '\n') {
-			cin.clear(), cout << "Date non valide. Veuillez SVP recommencer." << endl;
+      if (cin.peek() == '\n') {
+         cin.clear(), cout << "Date non valide. Veuillez SVP recommencer." << endl;
          cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
       } else {
-			cin >> annee;
+         cin >> annee;
          if (mois <= 12 && mois >= 1 && annee >= 1900 && annee <= 2100) {
             estValide = true;
          } else {
-            cin.clear(), cout << "Date non valide. Veuillez SVP recommencer." << endl;
+            cin.clear(), cout << "Date non valide. Veuillez SVP recommencer."
+                              << endl;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
          }
-         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
 
+      }
+      cout << mois << "-" << annee;
    } while (!estValide);
 }
